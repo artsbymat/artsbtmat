4b21e64c8923ad213067ad77e19fb3e7d60d0a62
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-10-19 18:30:23 +0700

    feat: add dynamic image size

--- src/components/public/Posts/img.jsx
+++ src/components/public/Posts/img.jsx
@@ -1,14 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import { RenderExcalidraw } from "./RenderExcalidraw";
 
 export const CustomImage = ({ src, alt = "" }) => {
   if (src.startsWith(`/content/Attachments/excalidraw`)) {
     return <RenderExcalidraw src={src} alt={alt} />;
-  } else {
-    return (
-      <div>
-        <Image src={src} width={1000} height={750} alt={alt} />
-      </div>
-    );
   }
+
+  const match = alt.match(/\((\d+)\)/);
+  const width = match ? parseInt(match[1], 10) : 1000;
+  const aspectRatio = 4 / 3;
+  const height = Math.round(width / aspectRatio);
+
+  const cleanAlt = alt.replace(/\s*\(\d+\)\s*/, "").trim();
+
+  return (
+    <div className="my-4 flex justify-center">
+      <div
+        style={{
+          width: `${width}px`,
+          height: "auto"
+        }}
+      >
+        <Image
+          src={src}
+          alt={cleanAlt}
+          width={width}
+          height={height}
+          style={{
+            width: "100%",
+            height: "auto",
+            objectFit: "contain"
+          }}
+        />
+      </div>
+    </div>
+  );
 };
